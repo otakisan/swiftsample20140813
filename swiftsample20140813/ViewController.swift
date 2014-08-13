829bb9e7d86a886ca68455ce89ec14ea83072c60
Takashi Ikeda <takashi@scorpius.local>
2014-08-13 13:37:14 +0900

    Initial Commit

---
+++ swiftsample20140813/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftsample20140813
+//
+//  Created by Takashi Ikeda on 2014/08/13.
+//  Copyright (c) 2014年 TI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
